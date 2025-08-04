61ce009a42f630d33f06ea9c6e1255a0816f0337
Mahdi Aghabeigi <mohammad.ladytarget@gmail.com>
2025-08-04 13:27:50 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // برای React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
