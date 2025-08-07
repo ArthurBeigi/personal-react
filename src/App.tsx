96097ec3500ca951c57bff9ef42ee724cfdd6139
hasan_lin <hasananesu22@gmail.com>
2025-08-07 17:53:21 +0330

    remaking the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setNum] = useState<number>(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className='bg-red-300' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-full h-screen space-y-2 flex flex-col justify-center items-center">
+      <button
+        onClick={() => {
+          setNum(num + 1);
+          console.log();
+        }}
+        className="w-40 h-20 rounded-xl border-2 border-red-200 bg-gray-200 transition-none focus:border-[3px] focus:border-pink-300 hover:border-green-200"
+      >
+        add number
+      </button>
+      <div className="bg-blue-200">the number is : {num}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
